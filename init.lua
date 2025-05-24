e2eff64b022be1a2170465f3b7104a40ec09251b
s4gor <imrans4gor@gmail.com>
2025-05-24 19:23:55 +0600

    initial push

---
+++ init.lua
@@ -0,0 +1,30 @@
+require "core.keymaps"
+require "core.options"
+
+vim.opt.number = true
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end
+
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+require('lazy').setup({
+
+{
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",  
+    "MunifTanjim/nui.nvim",
+  },
+}
+
+})
