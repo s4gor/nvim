b0bc672493d9a66332d1d5758d5d903a3aaf4ade
s4gor <imrans4gor@gmail.com>
2025-05-25 02:31:59 +0600

    enhanced editor with javascript plugins

--- init.lua
+++ init.lua
@@ -5,11 +5,15 @@ vim.opt.number = true
 vim.opt.autoindent = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+vim.opt.expandtab = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smarttab = true
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+	local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+	local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then
     error('Error cloning lazy.nvim:\n' .. out)
   end
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,5 +1,6 @@
 -- key map
 
+local opts = { noremap = true, silent = true }
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
@@ -7,23 +8,17 @@ vim.keymap.set("n", "<leader>e", function()
   require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
 end, { desc = "Toggle Neo-tree" })
 
-local opts = { noremap = true, silent = true }
 
--- Copy (yank) the selected lines in visual mode or current line in normal mode
 vim.keymap.set({"n", "v"}, "<leader>c", function()
   if vim.fn.mode() == 'v' or vim.fn.mode() == 'V' then
-    -- Visual mode: yank selection
     vim.cmd('normal! "+y')
   else
-    -- Normal mode: yank current line
     vim.cmd('normal! "+yy')
   end
 end, opts)
 
--- Paste from system clipboard in normal mode
 vim.keymap.set("n", "<leader>p", '"+p', opts)
 
--- Cut (delete and save to system clipboard) selected lines in visual mode or current line in normal mode
 vim.keymap.set({"n", "v"}, "<leader>x", function()
   if vim.fn.mode() == 'v' or vim.fn.mode() == 'V' then
     vim.cmd('normal! "+d')
@@ -32,3 +27,5 @@ vim.keymap.set({"n", "v"}, "<leader>x", function()
   end
 end, opts)
 
+vim.api.nvim_set_keymap('v', '<Tab>', '>>', opts)
+vim.api.nvim_set_keymap('v', '<S-Tab>', '<<', opts)
