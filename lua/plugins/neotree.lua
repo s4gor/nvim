6513b06e494478360cb27437af9882f8b2ec73b0
s4gor <imrans4gor@gmail.com>
2025-07-05 03:32:51 +0600

    added dot file support

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,3 +1,4 @@
+
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
@@ -6,4 +7,20 @@ return {
     "nvim-tree/nvim-web-devicons",  
     "MunifTanjim/nui.nvim",
   },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,         -- Show all items
+          hide_dotfiles = false,  -- Show dotfiles (like .env, .gitignore, etc.)
+          hide_gitignored = false,
+        },
+      },
+      window = {
+        mappings = {
+          ["H"] = "toggle_hidden",  -- Optional: Press H to toggle hidden files
+        },
+      },
+    })
+  end,
 }
